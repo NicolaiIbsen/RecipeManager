3fd30c4c94b38ed19f83e15a5302c339418b86a5
Mara-AspIT <mara@aspit.dk>
2017-11-07 21:38:54 +0100

    Implemented IngredientKind (note name change)

---
+++ RecipeManager.Entities/IngredientType.cs
@@ -0,0 +1,17 @@
+﻿namespace RecipeManager.Entities
+{
+    /// <summary>The types of ingredients for recipes.</summary>
+    public enum IngredientKind
+    {
+        Dairy,
+        Flour,
+        Meat,
+        Vegetables,
+        Fish,
+        RootVegetables,
+        Rice,
+        Pasta,
+        Conserves,
+        Mushrooms
+    }
+}
