a9420a58a370ce300c1f9214a059cfe8962d9ac0
Mara-AspIT <mara@aspit.dk>
2017-11-07 22:23:07 +0100

    Implemented Ingredient.

---
+++ RecipeManager.Entities/Ingredient.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using static System.String;
+
+namespace RecipeManager.Entities
+{
+    /// <summary>Represents an ingredient.</summary>
+    public class Ingredient
+    {
+        #region Fields
+        /// <summary>The unique id for the row in the database, that represents this object.</summary>
+        protected readonly int id;
+
+        /// <summary>the name of the ingredient</summary>
+        protected string name;
+
+        /// <summary>The price of the ingredient.</summary>
+        protected decimal price;
+
+        /// <summary>The general type of ingredient for this ingredient.
+        /// </summary>
+        protected IngredientKind ingredientKind;
+        #endregion
+
+
+        #region Properties
+        /// <summary>gets the id of this object.</summary>
+        public int Id => id;
+
+        public virtual string Name
+        {
+            get => name;
+            set
+            {
+                (bool isValid, string error) = IsNameValid(value);
+                if(!isValid)
+                    throw new ArgumentException(error, nameof(Name));
+                else if(name != value)
+                    name = value;
+            }
+        }
+
+        public virtual IngredientKind IngredientKind
+        {
+            get => ingredientKind;
+            set => ingredientKind = value;
+        }
+
+        public virtual decimal Price
+        {
+            get => price;
+            set
+            {
+                (bool isValid, string error) = IsPriceValid(value);
+                if(!isValid)
+                    throw new ArgumentException(error, nameof(Price));
+                else if(price != value)
+                    price = value;
+            }
+        }
+        #endregion
+
+
+        #region Methods
+        /// <summary>Validates that a name only consists of letters.</summary>
+        /// <param name="name">The name to validate.</param>
+        /// <returns>A <see cref="bool"/> indicating the validation result, and a <see cref="string"/> containg an error message.</returns>
+        public static (bool, string) IsNameValid(string name)
+            => name is null
+            ? (false, "objektreferencen ikke indstillet til en forekomst")
+            : !name.All(c => Char.IsLetter(c))
+            ? (false, "Indeholder enten ingen, eller andre karakterer end, bogstaver")
+            : (true, Empty);
+
+        public static (bool, string) IsPriceValid(decimal price)
+            => price > default(decimal) ? (true, Empty) : (false, "negativ værdi ikke tilladt");
+
+        public override string ToString()
+            => $"{(name.EndsWith("s") ? name : name + "s")} is a {ingredientKind} and costs {price.ToString("C")} per kg.";
+        #endregion
+    }
+}
